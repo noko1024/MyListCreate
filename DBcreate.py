88f2d6157b72c67f042a29ad50adfd961f616311
yugu-0202 <applerin1119@gmail.com>
2021-08-17 01:13:00 +0900

    fix

--- DBcreate.py
+++ DBcreate.py
@@ -4,9 +4,9 @@ import sqlite3
 conn = sqlite3.connect('niconico.db')
 c = conn.cursor()
 
-c.execute("create table if not exist tableDB(tag txt primary key,tableName txt,mylistCount int,mylistName txt)")
-c.execute("create table if not exist rmTable(id int primary key)")
-c.execute("create table if not exist buffer(id int primary key,mylistNum int,tag txt)")
+c.execute("create table if not exists tableDB(tag txt primary key,tableName txt,mylistCount int,mylistName txt)")
+c.execute("create table if not exists rmTable(id int primary key)")
+c.execute("create table if not exists buffer(id int primary key,mylistNum int,tag txt)")
 
 conn.commit()
 conn.close()
