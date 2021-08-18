917d8184112f5fd78eb987cda330f3833a92c352
yugu-0202 <applerin1119@gmail.com>
2021-08-18 16:57:49 +0900

    add comment out

--- niconico.py
+++ niconico.py
@@ -38,13 +38,19 @@ def StartUp():
     conn = sqlite3.connect("niconico.db")
     c = conn.cursor()
 
+    #buffer table に追加されているデータのタグ情報でfor分を回す
     for tag in c.execute("select tag from buffer"):
+        #そのタグのテーブルを探す
         c.execute("select tableName from tableDB where tag = '%s'" % tag[0])
-        table = c.fetchone()
-        c.execute("insert into %s(id,mylistNum) select id,mylistNum from buffer where tag = '%s'" % (table[0],tag[0]))
-        c.execute("select count(*) from %s" % table[0])
+        table = c.fetchone()[0]
+        #buffer table のデータをタグテーブルに移す
+        c.execute("insert into %s(id,mylistNum) select id,mylistNum from buffer where tag = '%s'" % (table,tag[0]))
+        #タグテーブルのデータ数を出す
+        c.execute("select count(*) from %s" % table)
+        #マイリスト登録数を更新する
         c.execute("update tableDB set mylistCount = %s" % c.fetchone()[0])
 
+    #buffer table 内のデータを全て削除する
     c.execute("delete from buffer")
     conn.commit()
     conn.close()
@@ -54,19 +60,26 @@ def DBcheck(tag):
     conn = sqlite3.connect("niconico.db")
     c = conn.cursor()
 
+    #タグテーブル名の取得
     c.execute("select tableName from tableDB where tag = '%s'" % tag)
     tableName = c.fetchone()
 
+    #タグテーブルが無かった場合
     if not tableName:
+        #ランダムな文字列を生成
         while True:
             name = ''.join(random.choices(string.ascii_letters, k=10))
+            #そのテーブルが存在するかをチェック
             c.execute("select count(*) from sqlite_master where type = 'table' and name = '%s'" % name)
             ch = int(c.fetchone()[0])
             if ch == 0:
                 break;
 
+        #テーブルの生成
         c.execute("create table %s(id int primary key,mylistNum int)" % name)
+        #インデックスを生成して検索速度をあげる
         c.execute("create index idindex on %s(id)" % name)
+        #テーブルDBにデータを追加する
         c.execute("insert into tableDB values ('%s','%s',%s,'%s')" % (tag,name,0,tag))
 
     conn.commit()
@@ -74,19 +87,22 @@ def DBcheck(tag):
 
 #htmlからのデータ取得
 def MainScraping(URL,mylistCount,mylistName):
-    if int(mylistCount/25000) >= 1:
-        name = "%sその%s" % (mylistName,int(mylistCount/25000))
+    if int(mylistCount/500) >= 1:
+        name = "%sその%s" % (mylistName,int(mylistCount/500))
     else:
         name = mylistName
     browser.get(rootURL + URL)
     time.sleep(0.5)
+    #タグ固定のチェック
     if TagCheck(tagName) == True:
         if mylistCount % 500 == 0:
+            #マイリストの生成&追加
             mylistCreate(name,mylistCount)
         else:
+            #マイリストへ追加
             mylistAdd(name)
         mylistCount += 1
-    time.sleep(10)
+
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
@@ -95,6 +111,8 @@ def MainScraping(URL,mylistCount,mylistName):
 
     conn.commit()
     conn.close()
+
+    time.sleep(10)
     print(URL[9:17]+"finish")
 
     return mylistCount
@@ -169,6 +187,7 @@ def DataBaseAdd(tableName):
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
+    #bufferからタグテーブルにデータを移す
     c.execute("insert into %s select id,mylistNum from buffer" % tableName)
 
     conn.commit()
@@ -179,15 +198,19 @@ def Authentication(tableName,id):
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
+    #タグテーブルからIDを検索
     c.execute("select id from %s where id = '%s'" % (tableName,id))
     ch = c.fetchone()
+    #除外テーブルからIDを検索
     c.execute("select id from rmTable where id = '%s'" % id)
     check = c.fetchone()
 
     print(check)
+    #除外テーブル、タグテーブルに該当した場合
     if check or ch:
         print("Authentication:True")
         return True
+    #どちらも該当しなかった場合
     else:
         print("Authentication:False")
         return False
@@ -200,6 +223,7 @@ def Add():
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
+    #おおもとのテーブルからテーブル名、マイリスト登録数、マイリスト名を取得
     c.execute("select tableName,mylistCount,mylistName from tableDB where tag = '%s'" % tagName)
     confList = c.fetchone()
 
@@ -219,6 +243,7 @@ def Add():
 
     while True:
         soup = BeautifulSoup(browser.page_source, "lxml")
+        #検索結果から動画のURLを抽出
         maindiv = soup.find("div",{"class":"contentBody video uad videoList videoList01"})
         if not maindiv:
             print("finish")
@@ -234,8 +259,10 @@ def Add():
 
         for URL in URLs:
             print(URL[9:17]+"start")
+            #マイリスト追加済みかどうか
             if Authentication(tableName,int(URL[9:17])) == True:
                 continue
+
             mylistCount = MainScraping(URL,mylistCount,mylistName)
 
         page += 1
@@ -243,6 +270,7 @@ def Add():
         browser.get(checkurl)
         time.sleep(10)
 
+    #データベースにbufferのデータを移す
     DataBaseAdd(tableName)
     browser.quit()
 
@@ -251,26 +279,28 @@ def IdAdd(id):
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
+    #削除テーブルに登録する
     c,execute("insert into rmTable valeus ('%s')" % id)
 
     conn.commit()
     conn.close()
-    print(pivot)
-    print(pivotPoint)
 
 #動画データのマイリス保存先検索
 def Check(id):
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
+    #除外登録済みのデータを探す
     c.execute("select id from rmTable where id = '%s'" % id)
     rmed = c.fetchone()
 
+    #削除登録済みだった場合
     if rmed:
         return "removed"
 
     ans = []
 
+    #マイリスト追加済みかを探す
     for tag in c.execute("select tag,tableName,mylistName from tableDB"):
         c.execute("select mylistNum from %s where id = '%s'" % (tag[1],id))
         myNum = c.fetchone()
@@ -278,20 +308,29 @@ def Check(id):
         if myNum:
             ans.append([tag[2],myNum[0]]) #[[mylistName,mylistNum]...]
 
+    #マイリスト追加済みだった場合
     if ans:
         return ans
+    #追加されていなかった場合
     else:
         return None
 
 #削除発火ポイント
 def Remove():
+    #削除登録するID
     passid = int(input("id>"))
+    #そのIDがどこのタグで登録済みチェックする
     answer = Check(passid)
+
+    #削除登録済みだった場合
     if answer == "removed":
         print("This ID is registered")
+    #削除登録されていなかった場合
     elif not answer:
         print("Register this ID")
+        #IDを除外登録する
         IdAdd(passid)
+    #既にマイリスト追加済みだった場合
     else:
         print("Already added\nPlease delete it manually\nmylistName")
         for mylist in answer:
@@ -299,16 +338,20 @@ def Remove():
                 print("\t%sその%s" % (mylist[0],mylist[1]))
             else:
                 print("\t%s" % mylist[0])
+            #IDを除外登録する
             IdAdd(passid)
 
 
+#テーブルの削除
 def RmTable():
     conn = sqlite3.connect('niconico.db')
     c = conn.cursor()
 
+    #タグ用テーブル名の取得
     c.execute("select tableName from tableDB where tag = '%s'" % tagName)
     tableName = c.fetchone()[0]
 
+    #テーブルと登録データの削除
     c.execute("drop table %s" % tableName)
     c.execute("delete from tableDB where tag = '%s'" % tagName)
 
@@ -316,18 +359,28 @@ def RmTable():
     conn.close()
 
 StartUp()
-#初期設定
+
+#mode選択
 mode = input("mode>")
+
+#マイリスト除外登録
 if mode == "remove":
     Remove()
 
+#探すタグの指定
 tagName = input("tagName>")
+
+#テーブルの削除
 if mode == "rmtable":
     RmTable()
 
+#ニコニコログイン用のデータの取得
 USER = input("LoginID>")
 PASS = getpass.getpass("LoginPassword>")
+
+#マイリスト追加
 if mode == "add":
+    #chrome driver の起動
     browser = webdriver.Chrome(cwd+"/lib/chromedriver.exe",chrome_options=options)
     browser.implicitly_wait(1)
     Add()
